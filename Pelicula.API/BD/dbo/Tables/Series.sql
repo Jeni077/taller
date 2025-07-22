758dc3c8ccd89e94affedc89b7b1b26b7759d414
Jeni077 <105757830+Jeni077@users.noreply.github.com>
2025-07-21 18:17:50 -0600

    updaste

---
+++ Pelicula.API/BD/dbo/Tables/Series.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Series] (
+    [IdSerie]      UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
+    [Titulo]       NVARCHAR (200)   NOT NULL,
+    [ImagenUrl]    NVARCHAR (MAX)   NULL,
+    [Descripcion]  NVARCHAR (MAX)   NULL,
+    [FechaEstreno] DATE             NULL,
+    [Calificacion] FLOAT (53)       NULL,
+    [IdGenero]     UNIQUEIDENTIFIER NOT NULL,
+    PRIMARY KEY CLUSTERED ([IdSerie] ASC),
+    FOREIGN KEY ([IdGenero]) REFERENCES [dbo].[Generos] ([IdGenero])
+);
+
---
+++ Pelicula.API/BD/dbo/Tables/VisualizacionesPendientes.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[VisualizacionesPendientes] (
+    [IdPendiente]   UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
+    [IdUsuario]     UNIQUEIDENTIFIER NOT NULL,
+    [IdPelicula]    INT              NULL,
+    [IdSerie]       INT              NULL,
+    [Prioridad]     INT              NOT NULL,
+    [FechaRegistro] DATETIME         DEFAULT (getdate()) NULL,
+    PRIMARY KEY CLUSTERED ([IdPendiente] ASC),
+    CHECK ([Prioridad]>=(1) AND [Prioridad]<=(3)),
+    CONSTRAINT [FK_Visualiza_IdUsuario] FOREIGN KEY ([IdUsuario]) REFERENCES [dbo].[Usuario] ([IdUsuario])
+);
+
